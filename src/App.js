7e6508f7e784730149d729c392c6cc24e790d437
dw012800 <dw012800@gmail.com>
2024-07-09 14:19:47 -0700

    first push

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='bg-green-100'>
+      <header className="text-8xl mt-5 mb-5">
+        Derrick Wong
+      </header>
+    
+<h1 className='text-xl ml-2 mr-2'>I am a software engineer who is focused on team agenda and roadmap coordination. My previous experience as an auditor and an operations staff member, I have adapted to many different situations and worked with people of many different skill sets. I want to use my skills in roadmap execution and adaptability in order to ramp team efficiency. Creation of a project from the ground up has its own reward, I wish to help teams to hit their targets in order to complete large-scale challenges.</h1>
+
+      <div className='flex flex-col items-center'>
+
+      <div className='flex flex-col '>
+      <a href='https://earthbound-roulette.netlify.app/'>
+        <div className='border-2 border-black rounded-xl mt-6 h-8 w-31 '>
+          Project 1: Earthbound Russian Roulette
+        </div>
+      </a>
+      <a href='https://job-project-3ab6449a2dd6.herokuapp.com/'>
+        <div className='border-2 border-black rounded-xl mt-4 h-8 w-31 flex flex-col items-center'>
+          Project 2: Job Board App
+        </div>
+      </a>
+      <a href='https://dth-bank.netlify.app'>
+        <div className='border-2 border-black rounded-xl mt-4 h-8 w-31 flex flex-col items-center'>
+        Project 3: DTH National Bank
+        </div>
+      </a>
+      <a href='https://main--tk7frames.netlify.app/'>
+        <div className='border-2 border-black rounded-xl mt-4 h-8 w-31 flex flex-col items-center'>
+        Project 4: Tekken 7 Frame Data Sheet
+        </div>
+      </a>
+      </div>
+      
+      <div>
+      <a href='https://docs.google.com/document/d/1WvFIMyjO7_a80FdG2V7zZkWYjN6l0RTkptNQec15wKc/edit'>
+        <div className='border-2 border-black mt-4 w-20 flex flex-col items-center'>
+        Resume
+        </div>
+      </a>
+      <a href='https://github.com/dw012800'>
+        <div className='border-2 border-black mt-4 w-20 flex flex-col items-center'>
+        Github
+        </div>
+      </a>
+      <a href='https://www.linkedin.com/in/derrick-wong-8392791b8/'>
+        <div className='border-2 border-black mt-4 w-20 flex flex-col items-center'>
+        Linkedin
+        </div>
+      </a>
+      </div>
+      </div>
+
+
+    </div>
+  );
+}
+
+
+export default App;
